ab287b53c96a2cf754ae3aeeba4ffb32cf0e3fb0
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-01-18 16:45:45 +0100

    Enhance update check functionality and format release tag name

--- LrGeniusTagAI.lrdevplugin/UpdateCheck.lua
+++ LrGeniusTagAI.lrdevplugin/UpdateCheck.lua
@@ -2,12 +2,35 @@ require "Info"
 
 UpdateCheck = {}
 
-UpdateCheck.releaseTagName = tostring(Info.MAJOR) .. "." .. tostring(Info.MINOR) .. "." .. tostring(Info.REVISION)
+UpdateCheck.releaseTagName = "v" .. tostring(Info.MAJOR) .. "." .. tostring(Info.MINOR) .. "." .. tostring(Info.REVISION)
+UpdateCheck.updateCheckUrl = "https://api.github.com/repos/LrGenius/LrGeniusTagAI/releases/latest"
+UpdateCheck.latestReleaseUrl = "https://github.com/LrGenius/LrGeniusTagAI/releases/latest"
 
 function UpdateCheck.checkForNewVersion()
+    local response, headers = LrHttp.get(UpdateCheck.updateCheckUrl)
+
+    if headers.status == 200 then
+        if response ~= nil then
+            local decoded = JSON:decode(response)
+            if decoded ~= nil then
+                if decoded.tag_name ~= UpdateCheck.releaseTagName then
+                    LrHttp.openUrlInBrowser(UpdateCheck.latestReleaseUrl)
+                else
+                    LrDialogs.message("You're on the latest plugin version: " .. UpdateCheck.releaseTagName)
+                end
+            end
+        else
+            log:error('Could not run update check. Empty response')
+        end
+    else
+        log:error('Update check failed. ' .. UpdateCheck.updateCheckUrl)
+        log:error(Util.dumpTable(headers))
+        log:error(response)
+        return nil
+    end
     return nil
 end
 
 function UpdateCheck.checkForNewVersionInBackground()
-    return nil
+    return UpdateCheck.checkForNewVersion()
 end
